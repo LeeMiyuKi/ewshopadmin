f74478c305a06468e40ca8470cf612c68ba273d7
lmx <lmx@.com>
2023-02-15 16:34:34 +0800

    引入Naive UI.配置组件自动导入

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -11,8 +7,16 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <div>
+    <NButton>123123</NButton>
+  </div>
+
 </template>
+
+<script setup lang="ts">
+
+</script>
 
 <style scoped>
 .logo {
